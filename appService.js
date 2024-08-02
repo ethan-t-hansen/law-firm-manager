c847c86a0e9b05bcb4edeac1b934460f4a093f63
Fua fua pancake <114277161+elisekat@users.noreply.github.com>
2024-08-01 17:07:06 -0700

    removed init statements

--- appService.js
+++ appService.js
@@ -241,349 +241,6 @@ async function fetchCasesTableFromDb() {
     });
 }
 
-async function initInsTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE INSURANCETABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-        const result = await connection.execute(
-            `CREATE TABLE INSURANCETABLE(
-            PolicyNum INT,
-	        ExpiryDate DATE NOT NULL,
-	        ClientID INT,
-	        FOREIGN KEY (ClientID) REFERENCES Client(ClientID)
-		    ON UPDATE CASCADE,
-	        PRIMARY KEY (PolicyNum, ClientID)
-            )`
-        );
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
-
-async function initClientTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE CLIENTTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-        const result = await connection.execute(
-            `CREATE TABLE CLIENTTABLE(
-            ClientID INT PRIMARY KEY,
-	        PhoneNum CHAR(10) NOT NULL,
-	        Name VARCHAR(30) NOT NULL,
-	        Email VARCHAR(30) NOT NULL,
-	        DateOfBirth CHAR(10),
-            UNIQUE(Email))`
-        );
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initOfficerTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE OFFICERTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-        const result = await connection.execute(
-            `CREATE TABLE OFFICERTABLE(
-	        OfficerID INT PRIMARY KEY,
-	        Department VARCHAR(30) NOT NULL,
-	        Name VARCHAR(30) NOT NULL
-            )`
-        );
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
-async function initTicketTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE TICKETTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-        const result = await connection.execute(
-            `CREATE TABLE TICKETTABLE(
-	        	TicketNum INT PRIMARY KEY,
-	            DateIssued DATE NOT NULL,
-	            Amount DECIMAL(12,2) NOT NULL,
-	            OfficerID INT NOT NULL,
-	            CaseID INT,
-	            City VARCHAR(30),
-	            StatuteCode INT,
-	            FOREIGN KEY (OfficerID) REFERENCES Officer(OfficerID)
-		        ON UPDATE CASCADE,
-                FOREIGN KEY (CaseID) REFERENCES Case(CaseID)
-		        ON UPDATE CASCADE,
-                FOREIGN KEY (City) REFERENCES TicketLocation(City)
-		        ON UPDATE CASCADE,
-                FOREIGN KEY (StatuteCode) REFERENCES TicketTypes(StatuteCode)
-		        ON UPDATE CASCADE)
-                )`
-        );
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
-
-async function initTicketLocTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE TICKETLOCTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE TICKETLOCTABLE(
-               City VARCHAR(30) PRIMARY KEY,
-              County VARCHAR(30) NOT NULL
-            )`
-        );
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
-async function initTicketTypesTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE TICKETTYPESTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE TICKETTYPESTABLE(
-            StatuteCode INT PRIMARY KEY,
-            TicketType VARCHAR(10)
-            )`
-        );
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
-
-async function initSpeedingTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE SPEEDINGTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE SPEEDINGTABLE(
-                TicketNum INT PRIMARY KEY,
-                TicketType VARCHAR(10),
-                SpeedingZone VARCHAR(30),
-                FOREIGN KEY (TicketNum) REFERENCES TICKETTABLE(TicketNum),
-                FOREIGN KEY (SpeedingZone) REFERENCES ZONETABLE(SpeedingZone)
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
-async function initZoneTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE ZONETABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE ZONETABLE(
-                SpeedingZone VARCHAR(30) PRIMARY KEY,
-                SpeedLimit INT
-            )   
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initParkingTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE PARKINGTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE PARKINGTABLE (
-                TicketNum INT PRIMARY KEY,
-                ParkingZone VARCHAR(30),
-                FOREIGN KEY (TicketNum) REFERENCES TICKETTABLE(TicketNum),
-
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initTrafficLightTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE TRAFFICLIGHTTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE TRAFFICLIGHTTABLE (
-                TicketNum INT PRIMARY KEY,
-                PhotoURL VARCHAR(50),
-                FOREIGN KEY (TicketNum) REFERENCES TICKETTABLE(TicketNum),
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initCourtTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE COURTTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE COURTTABLE (
-                CourtName VARCHAR(30) PRIMARY KEY,
-                Location VARCHAR(30) NOT NULL,
-                Type VARCHAR(30)
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initJudgeTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE JUDGETABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE JUDGETABLE (
-                JudgeID INT PRIMARY KEY,
-                Name VARCHAR(30),
-                CourtName VARCHAR(30),
-                FOREIGN KEY (CourtName) REFERENCES COURTTABLE(CourtName)
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initProsecutorTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE PROSECUTORTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE PROSECUTORTABLE (
-                ProsecutorID INT PRIMARY KEY,
-                Name VARCHAR(30),
-                CourtName VARCHAR(30),
-                FirmName VARCHAR(30),
-                FOREIGN KEY (CourtName) REFERENCES COURTTABLE(CourtName),
-                FOREIGN KEY (FirmName) REFERENCES FIRMTABLE(FirmName)
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initFirmEmploymentTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE FIRMTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE FIRMTABLE (
-                FirmName VARCHAR(30) PRIMARY KEY,
-                Clerk VARCHAR(30)
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-async function initCaseTable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE CASETABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE CASETABLE (
-                CaseID INT PRIMARY KEY,
-                DateFiled DATE,
-                HearingDate DATE,
-                CourtName VARCHAR(30),
-                ProsecutorID INT,
-                JudgeID INT,
-                TicketNum INT NOT NULL,
-                ClientID INT NOT NULL,
-                Outcome VARCHAR(25),
-                FOREIGN KEY (CourtName) REFERENCES COURTTABLE(CourtName)
-                    ON UPDATE CASCADE,
-                FOREIGN KEY (ProsecutorID) REFERENCES PROSECUTORTABLE(ProsecutorID)
-                    ON UPDATE CASCADE,
-                FOREIGN KEY (JudgeID) REFERENCES JUDGETABLE(JudgeID)
-                    ON UPDATE CASCADE,
-                FOREIGN KEY (TicketNum) REFERENCES TICKETTABLE(TicketNum)
-                    ON UPDATE CASCADE,
-                FOREIGN KEY (ClientID) REFERENCES CLIENTTABLE(ClientID)
-                    ON UPDATE CASCADE,
-                UNIQUE(TicketNum)
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
 // OG insert demoTable fn
 // async function insertDemoTable(id, name) {
 //     return await withOracleDB(async (connection) => {
@@ -831,47 +488,35 @@ async function deleteCase(caseID) {
     });
 }
 
-async function countDemoTable() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT Count(*) FROM DEMOTABLE');
-        return result.rows[0][0];
-    }).catch(() => {
-        return -1;
-    });
-}
+// async function countDemoTable() {
+//     return await withOracleDB(async (connection) => {
+//         const result = await connection.execute('SELECT Count(*) FROM DEMOTABLE');
+//         return result.rows[0][0];
+//     }).catch(() => {
+//         return -1;
+//     });
+// }
 
 module.exports = {
     testOracleConnection,
-
-    // fetchInsTableFromDb,
+    fetchInsTableFromDb,
     fetchClientTableFromDb,
-    // fetchAllTablesFromDb,
-
-    initInsTable,
-    initClientTable,
-    initOfficerTable,
-    initTicketTable,
-    initTicketLocTable,
-    initTicketTypesTable,
-    initSpeedingTable,
-    initZoneTable,
-    initParkingTable,
-    initTrafficLightTable,
-    initCourtTable,
-    initJudgeTable,
-    initProsecutorTable,
-    initFirmEmploymentTable,
-    initCaseTable,
-    
-    
-    insertClientTable,
+    fetchTicketTableFromDb,
+    fetchOfficerTableFromDb,
+    fetchTicketLocTableFromDb,
+    fetchTicketTypesTableFromDb,
+    fetchSpeedingTableFromDb,
+    fetchZoneTableFromDb,
+    fetchParkingTableFromDb,
+    fetchTrafficLightTableFromDb,
+    fetchCourtTableFromDb,
+    fetchJudgeTableFromDb,
+    fetchProsecutorTableFromDb,
+    fetchFirmTableFromDb,
+    fetchCasesTableFromDb,
     insertCaseTable,
-
- 
+    insertClientTable,
     updateClientTable,
     updateCaseTable,
-
-    // countDemoTable,
-
-    deleteCase
+    deleteCase,
 };
